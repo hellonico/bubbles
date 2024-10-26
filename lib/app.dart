7884cf194dca374de64af12f57de57bb9521ae21
Nicolas Modrzyk <hellonico@gmail.com>
2024-10-26 10:16:13 +0900

    persist application settings

--- lib/app.dart
+++ lib/app.dart
@@ -1,16 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AppSettings extends ChangeNotifier {
   bool showStarredTasks = false;
   bool showNonCompletedTasks = false;
 
-  void toggleShowStarredTasks() {
+  AppSettings() {
+    _loadSettings();
+  }
+
+  void _loadSettings() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    showStarredTasks = prefs.getBool('showStarredTasks') ?? false;
+    showNonCompletedTasks = prefs.getBool('showNonCompletedTasks') ?? false;
+    notifyListeners();
+  }
+
+  void toggleShowStarredTasks() async {
     showStarredTasks = !showStarredTasks;
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setBool('showStarredTasks', showStarredTasks);
     notifyListeners(); // Notify listeners when the value changes
   }
 
-  void toggleShowNonCompletedTasks() {
+  void toggleShowNonCompletedTasks() async {
     showNonCompletedTasks = !showNonCompletedTasks;
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setBool('showNonCompletedTasks', showNonCompletedTasks);
     notifyListeners();
   }
 }
